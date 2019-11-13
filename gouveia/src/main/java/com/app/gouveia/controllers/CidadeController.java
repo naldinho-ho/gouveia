9c9af59b206cbcf1344fc35051c1f66db1a50525
Aluno <Aluno@172.30.1.41>
2019-11-12 23:16:23 -0200

    Cidade

---
+++ gouveia/src/main/java/com/app/gouveia/controllers/CidadeController.java
@@ -0,0 +1,77 @@
+package com.app.gouveia.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.app.gouveia.models.Cidade;
+import com.app.gouveia.models.Estado;
+import com.app.gouveia.repository.CidadeRepository;
+import com.app.gouveia.repository.EstadoRepository;
+
+
+@Controller
+public class CidadeController {
+	@Autowired
+	private CidadeRepository repository;
+	
+	@Autowired
+	private EstadoRepository repositoryEstado;
+	
+	@GetMapping("administrativo/cidade/cidades")
+	public ModelAndView buscarTodos() {
+		ModelAndView mv = new ModelAndView("/administrativo/cidade/cidadeLista");
+		mv.addObject("cidades", repository.findAll());
+		return mv;
+	}
+	
+	@GetMapping("/administrativo/cidade/adicionarCidade")
+	public ModelAndView add(Cidade cidade) {
+		ModelAndView mv = new ModelAndView("/administrativo/cidade/cidadeAdicionar");
+		mv.addObject("cidade", cidade);
+		List<Estado> listaEstado = repositoryEstado.findAll();
+		mv.addObject("estados",listaEstado);
+		return mv;
+	}
+	
+	@GetMapping("/administrativo/cidade/editarCidade/{id}")
+	public ModelAndView edit(@PathVariable("id") Long id) {
+		
+		Optional<Cidade> cidade = repository.findById(id);
+		Cidade e = cidade.get();	
+		
+		return add(e);
+	}
+	
+	@GetMapping("/administrativo/cidade/removerCidade/{id}")
+	public ModelAndView delete(@PathVariable("id") Long id) {
+		
+		Optional<Cidade> cidade = repository.findById(id);
+		Cidade e = cidade.get();
+		repository.delete(e);	
+		
+		return buscarTodos();
+	}
+
+	@PostMapping("/administrativo/cidade/salvarCidade")
+	public ModelAndView save(@Valid Cidade cidade, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return add(cidade);
+		}
+		
+		repository.saveAndFlush(cidade);
+		
+		return buscarTodos();
+	}
+
+}
---
+++ gouveia/src/main/java/com/app/gouveia/repository/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.app.gouveia.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.gouveia.models.Cidade;
+
+
+public interface CidadeRepository extends JpaRepository<Cidade, Long>{
+
+}
+
